31f3a8c95994cd0ea6b77ad1ef6b341c641dc239
reborn307 <holarisaac09@gmail.com>
2025-12-25 19:40:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -102,5 +102,3 @@ def read_data():
     except Exception as e:
         return jsonify({"status":"error","message":str(e)}),500
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
